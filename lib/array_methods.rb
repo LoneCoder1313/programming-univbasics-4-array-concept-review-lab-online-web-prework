4a8cf16ac29eb6af7cff07a8adf9fb4aae0547ed
Ricky dildy <rdildy92@gmail.com>
2019-09-23 02:51:07 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -5,8 +5,8 @@ def a = [1,2,3,4,5]
   #=> FALSE
 end
 
-def find_max_value(array)
-  # Add your solution here
+def find_max_value([1,2,1,3,4,3,5,4,3,2,1])
+  #=> (5)
 end
 
 def find_min_value(array)
